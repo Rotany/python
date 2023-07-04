f6e422a222263c670d6759d2f2887295ea486a1c
Rotany <Rotany38@gmail.com>
2023-07-04 18:59:57 +0200

    creando_funciones

--- app/math.py
+++ app/math.py
@@ -8,3 +8,27 @@ print(suma (1, 2) * 2)
 print(suma(b= 10))
 print(suma(1))
 
+def resta(a = 9, b = 40, c = 5):
+    return a+b-c
+
+print(resta() * 5)
+print(resta(1, 8, 50))
+print(resta(b= 4))
+print(resta(10, 20, 5) /5)
+
+
+def div(a = 40, b = 10, c = 5):
+    return a+b/c
+print(div() * 5)
+print(div(20, 50,2)*6)
+print(div(c = 5))
+print(div(1))
+
+
+def mult(a = 8, b = 50):
+    return a*b
+
+print(mult()**2)
+print(mult(8, 50) / 4)
+print(mult(100, 50) +50)
+print(mult(50)*8)
