fe31656ea1642b15e64401efee5896ee398631ec
Rotany <Rotany38@gmail.com>
2023-07-05 17:05:04 +0200

    suiguiendo_con _def

--- app/math.py
+++ app/math.py
@@ -2,8 +2,8 @@ def suma( a = 5, b = 20):
     return a+b
 print (suma() * 2)
 
-print (suma(1, 2))
-print(suma (1, 2) * 2)
+print( suma(1, 2))
+print( suma (1, 2) * 2)
 
 print(suma(b= 10))
 print(suma(1))
@@ -17,18 +17,28 @@ print(resta(b= 4))
 print(resta(10, 20) /5)
 
 
-def div(a = 40, b = 5):
-    return a/b
+def div(a = 40, b = 5 ):
+    try:
+      return a/b  
+      
+    except ZeroDivisionError as error:
+       raise  Exception ('que pasa esto es un error')
+    
 print(div() * 5)
 print(div(20, 50)*6)
 print(div(b = 2))
-try:
-  print(div(b=0))
-except ZeroDivisionError as error:
-    print('que pasa esto es un error')
 
-
-
+  
+ 
+numbers = [1, 2, 3, 5, 6]  
+def multiplicar_array_por_dos(lista): 
+    datos=[]
+    for number in lista:
+        print(number * 2)
+        datos.append(number*2)
+    return datos
+print(multiplicar_array_por_dos(lista = numbers))
+ 
 
 def mult(a = 8, b = 50):
     return a*b
@@ -36,4 +46,6 @@ def mult(a = 8, b = 50):
 print(mult()**2)
 print(mult(8, 50) / 4)
 print(mult(100, 50) +50)
-print(mult(50)-8)
+print(mult(50))
+print(mult(b=0))
+print(div(b=0))
